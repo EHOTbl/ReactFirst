85840ab01a2a5ba1914302e17e5fd175f72bddac
EHOTbl <stalker_mogilev@mail.ru>
2023-02-16 20:55:43 +0300

    create prj

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    debugger
+    return (
+        <div>
+            App component
+            <Rating/>
+            <Accordeon/>
+        </div>
+    );
 }
 
+function Hello() {
+    debugger
+    return console.log("hello")
+}
+
+// Hello();
+
 export default App;
+
+function Rating() {
+    debugger
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    )
+}
+
+function Accordeon() {
+    debugger
+    return <div>
+        <h3>Menu</h3>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    </div>
+}
+
+function Star() {
+    return <div>Star</div>
+}
